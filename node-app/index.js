c7cc4f3caa598341da0f70b664eda039d2482f79
nick-jonas <njonas45@gmail.com>
2016-02-01 13:01:18 -0500

    parse result

--- node-app/index.js
+++ node-app/index.js
@@ -4,11 +4,19 @@ google_speech.ASR({
     developer_key: 'AIzaSyAbx9tR74Gh-pDTy_gsbsdt-s_pX5jK0ik',
     file: 'data/sample.wav',
     lang: 'en-US'
-  }, function(err, httpResponse, xml){
+  }, function(err, httpResponse, result){
     if(err){
         console.log(err);
       }else{
-        console.log(httpResponse.statusCode, xml)
+        console.log(httpResponse.statusCode, result);
+        // parse result
+        var result = '{"result":[{"alternative":[{"transcript":"testing testing hello 123 hello testing"},{"transcript":"testing testing hello 1 2 3 hello testing"},{"transcript":"testing testing hello 1 2 3 hello texting"},{"transcript":"testing testing hello 1 2 3a hello testing"}],"final":true}],"result_index":0}';
+        result = result.match(/transcript.*}/)[0];
+        result = '"' + result;
+        result = result.match(/"((?:\\.|[^"\\])*)"/g)[1].replace(/"/g, ''));
+        
       }
     }
 );
+
+
