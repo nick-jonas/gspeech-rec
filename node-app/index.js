d4a397a280827aae05f726b0d5121e83db978949
nick-jonas <njonas45@gmail.com>
2016-02-03 13:25:55 -0500

    test

--- node-app/index.js
+++ node-app/index.js
@@ -2,20 +2,16 @@ var fs        = require('fs'),
     request   = require('request'),
     qs        = require('querystring'),
     program   = require('commander'),
-    google_speech = require('google-speech'),
-    hasbin    = require('hasbin'),
     exec      = require('child_process').exec,
-    rec       = require('./record'),
     lang      = require('./lang'),
-    stt       = require('./stt'),
+    Stt       = require('./stt'),
+    Tts       = require('./tts'),
     Screen       = require('./screen');
     
-var WIT_TOKEN = 'JIO3NKSOIXLMGI5TZJ3FJVZDJ2GNRTL2';
-var GOOGLE_KEY = 'AIzaSyAUpnxV2S7nNAlIo9sZnJBVligAJBzMWc0';
-var SAMPLE_RATE = 44100;
+// var WIT_TOKEN = 'JIO3NKSOIXLMGI5TZJ3FJVZDJ2GNRTL2';
+// var GOOGLE_KEY = 'AIzaSyAUpnxV2S7nNAlIo9sZnJBVligAJBzMWc0';
+// var SAMPLE_RATE = 44100;
 
-var OUTPUT_TYPES = ['google', 'wit', 'file'];
-var outputType = 'file';
 var inLanguage = lang.getCodeFromName('English');
 var outLanguage = lang.getCodeFromName('Spanish');
 
@@ -23,62 +19,10 @@ var outLanguage = lang.getCodeFromName('Spanish');
 // program parameter definition
 program
   .version('0.0.1')
-  .option('-o --output [value]', 'Output type: ' + OUTPUT_TYPES)
+  .option('-o --output [value]', 'Output type: google, wit, file')
   .option('-r --sample-rate <n>', 'Sample rate for recording: 16000, 44100, 48000')
   .parse(process.argv);
 
-if(program.output){
-  if(OUTPUT_TYPES.indexOf(program.output) < 0){
-    console.warn('Warning: output type ' + program.output + ' needs to be one of the following: ' + OUTPUT_TYPES);
-  }else{
-    outputType = program.output;
-  }
-}
-if(program.sampleRate){
-  console.log('setting sample rate to: ' + program.sampleRate);
-  SAMPLE_RATE = program.sampleRate;
-}
-
-// piped output options
-var fileRequest = function(){ return fs.createWriteStream('data/sample.wav', { encoding: 'binary' }); };
-var witRequest = function(){ 
-  return request.post({
-      'url'     : 'https://api.wit.ai/speech?client=chromium&lang=' + inLanguage + '&output=json',
-      'headers' : {
-        'Accept'        : 'application/vnd.wit.20160202+json',
-        'Authorization' : 'Bearer ' + WIT_TOKEN,
-        'Content-Type'  : 'audio/wav'
-      }
-  }, exports.parseResult);
-}
-var googleRequest = function(){
-  var params = {};
-  params['key'] = GOOGLE_KEY;
-  params['lang'] = inLanguage;
-  params['output'] = 'json';
-  return request.post({
-      'url' : 'https://www.google.com/speech-api/v2/recognize?' + qs.stringify(params),
-      'headers' : {
-        'Content-Type': 'audio/l16; rate=16000;'
-      }
-  }, exports.parseResult);
-}
-
-
-
-// on press (or looped), start recording
-var startRecording = function(){
-  var pipedOutput = fileRequest;
-  if(outputType === 'google'){ pipedOutput = googleRequest; }
-  if(outputType === 'wit'){ pipedOutput = witRequest; }
-
-  rec.start({
-    sampleRate : SAMPLE_RATE,
-    verbose : true,
-    threshold: 3
-  })
-  .pipe(pipedOutput());
-}
 
 var translate = function(body){
   var params = {};
@@ -88,8 +32,8 @@ var translate = function(body){
   return request.get({
     'url': 'https://www.googleapis.com/language/translate/v2?' + qs.stringify(params)
   }, onTranslateComplete);
-
 }
+
 
 var onTranslateComplete = function (err, resp, body){
   if(err){
@@ -115,78 +59,19 @@ var onTranslateComplete = function (err, resp, body){
 
 }
 
-var textToSpeech = function(body){
-  google_speech.TTS({
-    text: body,
-    file: 'data/out.mp3',
-    language: outLanguage
-    }, function(){
-      console.log('Created data/out.mp3 with text-to-speech.');
-      // if omxplayer exists, sound it out
-      hasbin('omxplayer', function(result){
-        if(result){
-          var cmd = 'omxplayer data/out.mp3';
-          exec(cmd, function(error, stdout, stderr){
-            console.log(stdout);
-            setTimeout(startRecording, 2000);
-          });
-        }else{
-          console.log('omxplayer is not installed, will not playback audio.');
-          setTimeout(startRecording, 2000);
-        }
-      });      
 
-    }
-  );
+var stt = new Stt({
+  provider: program.output, 
+  onResult: onSpeechToTextResponse, 
+  sampleRate: program.sampleRate
+});
+
+stt.start(inLanguage);
+
+function onSpeechToTextResponse(result){
+  console.log(result);
 }
 
-// parse result, dependent on piped output
-exports.parseResult = function (err, resp, body) {
-  var result;
-  console.log(body);
-  if(typeof body === 'undefined' || !body || body === ''){
-    console.log('Could not capture any audio data. Please set up microphone correctly.');
-    return;
-  }
-  // parse output
-  if(outputType === 'google'){
-    // Example Google Response
-    // '{"result":[{"alternative":[{"transcript":"testing testing hello 123 hello testing"},{"transcript":"testing testing hello 1 2 3 hello testing"},{"transcript":"testing testing hello 1 2 3 hello texting"},{"transcript":"testing testing hello 1 2 3a hello testing"}],"final":true}],"result_index":0}';
-    if(!body.match(/transcript.*}/)){
-      console.log('Could not capture any audio data. Please set up microphone correctly.');
-      return;
-    }
-    result = body.match(/transcript.*}/)[0];
-    result = '"' + result;
-    result = result.match(/"((?:\\.|[^"\\])*)"/g)[1].replace(/"/g, '');
-  }else if(outputType === 'wit'){
-    /* Example Wit.ai response:
-      {
-        "msg_id" : "9311660f-6810-4616-a0bd-246768a6b515",
-        "_text" : "hi testing hello one two",
-        "outcomes" : [ {
-          "_text" : "hi testing hello one two",
-          "confidence" : 0.51,
-          "intent" : "test",
-          "entities" : { }
-        } ]
-      }
-    */
-    result = JSON.parse(body);
-    result = result['_text'];
-  }else{
-    console.log('Your sound file has been saved to data/sample.wav.')
-  }
-
-  console.log(result);
-
-  // translate
-  var translatedText = translate(result);
-
-};
-
-
-startRecording();
 
 // try{
 //   var lcd = new Screen({
--- node-app/stt.js
+++ node-app/stt.js
@@ -8,12 +8,113 @@
   to the file system and referenced instead of piped to 3rd party API
 
 */
-var Stt = function(provider){
-  this.provider = provider;
+
+var request   = require('request'),
+  qs          = require('qs'),
+  fs          = require('fs'),
+  rec         = require('./record'),
+  lang        = require('./lang'); 
+
+var WIT_TOKEN = 'JIO3NKSOIXLMGI5TZJ3FJVZDJ2GNRTL2';
+var GOOGLE_KEY = 'AIzaSyAUpnxV2S7nNAlIo9sZnJBVligAJBzMWc0';
+
+var sampleRate;
+var provider;
+var onResultCallback;
+
+/*
+
+options: {
+  provider: <string> google|wit|file
+  onResult: <fn> returns a string of text
+  sampleRate: <n> an integer defining sample rate (i.e. 16000, 44100, 48000)
+}
+
+*/
+var Stt = function(options){
+  sampleRate = options.sampleRate || 16000;
+  onResultCallback = options.onResult;
+  provider = options.provider || 'google';
 };
 
-Stt.prototype.start = function() {
-  // body...
+Stt.prototype.start = function(inLang) {
+  var pipedOutput = fileRequest;
+  if(provider === 'google'){ pipedOutput = googleRequest; }
+  if(provider === 'wit'){ pipedOutput = witRequest; }
+  console.log('Starting recording using provider: ' + provider);
+  var recording = rec.start({
+    sampleRate : sampleRate,
+    verbose : true,
+    threshold: 3
+  })
+  .pipe(pipedOutput(inLang));
 };
+
+
+// piped output options
+var fileRequest = function(inLang){ return fs.createWriteStream('data/sample.wav', { encoding: 'binary' }); };
+var witRequest = function(inLang){ 
+  return request.post({
+      'url'     : 'https://api.wit.ai/speech?client=chromium&lang=' + inLang + '&output=json',
+      'headers' : {
+        'Accept'        : 'application/vnd.wit.20160202+json',
+        'Authorization' : 'Bearer ' + WIT_TOKEN,
+        'Content-Type'  : 'audio/wav'
+      }
+  }, parseResult);
+}
+var googleRequest = function(inLang){
+  var params = {};
+  params['key'] = GOOGLE_KEY;
+  params['lang'] = inLang;
+  params['output'] = 'json';
+  return request.post({
+      'url' : 'https://www.google.com/speech-api/v2/recognize?' + qs.stringify(params),
+      'headers' : {
+        'Content-Type': 'audio/l16; rate=' + sampleRate + ';'
+      }
+  }, parseResult);
+}
+
+var parseResult = function (err, resp, body) {
+  var result;
+  if(typeof body === 'undefined' || !body || body === ''){
+    console.log('Could not capture any audio data. Please set up microphone correctly.');
+    console.log(body);
+    return;
+  }
+  // parse output
+  if(provider === 'google'){
+    // Example Google Response
+    // '{"result":[{"alternative":[{"transcript":"testing testing hello 123 hello testing"},{"transcript":"testing testing hello 1 2 3 hello testing"},{"transcript":"testing testing hello 1 2 3 hello texting"},{"transcript":"testing testing hello 1 2 3a hello testing"}],"final":true}],"result_index":0}';
+    if(!body.match(/transcript.*}/)){
+      console.log('Could not capture any audio data. Please set up microphone correctly.');
+      return;
+    }
+    result = body.match(/transcript.*}/)[0];
+    result = '"' + result;
+    result = result.match(/"((?:\\.|[^"\\])*)"/g)[1].replace(/"/g, '');
+  }else if(provider === 'wit'){
+    /* Example Wit.ai response:
+      {
+        "msg_id" : "9311660f-6810-4616-a0bd-246768a6b515",
+        "_text" : "hi testing hello one two",
+        "outcomes" : [ {
+          "_text" : "hi testing hello one two",
+          "confidence" : 0.51,
+          "intent" : "test",
+          "entities" : { }
+        } ]
+      }
+    */
+    result = JSON.parse(body);
+    result = result['_text'];
+  }else{
+    console.log('Your sound file has been saved to data/sample.wav.')
+  }
+
+  onResultCallback(result);
+}
+
 
 module.exports = Stt;
